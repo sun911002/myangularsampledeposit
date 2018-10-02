3220826a14d64450fc032041d9dd2b36883956e6
sundar86 <sundarammcse@gmail.com>
2018-10-02 19:44:59 +0530

    angular sample project
    we add the sample routing and sharng the services

---
+++ src/app/modelform/modelform.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModelformComponent } from './modelform.component';
+
+describe('ModelformComponent', () => {
+  let component: ModelformComponent;
+  let fixture: ComponentFixture<ModelformComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ModelformComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModelformComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
